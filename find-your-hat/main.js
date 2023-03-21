c684fb7b0271fdd050abedd7cf1a29bacab00126
Duncanwar <semunda9@gmail.com>
2023-03-22 00:02:54 +0200

    find your har

---
+++ find-your-hat/main.js
@@ -0,0 +1,103 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+  constructor(field=[[]]){
+    this.field= field
+    this.locationX= 0
+    this.locationY= 0
+    this.field[0][0]=pathCharacter
+  }
+
+    runGame(){
+      let playing= true
+      while(playing){
+      this.print()
+      this.askQuestion()
+        if(!this.isInBounds()){
+console.log("Out of bounds")
+          playing= false
+          break;
+        }
+        else if(this.isHole()){
+          playing=false
+          console.log('Sorry you fell in a Hole')
+          break;
+        }
+        else if(this.isHat()){
+          console.log('Congratulation you found it')
+          playing= false
+          break;  
+        }
+this.field[this.locationY][this.locationX]= pathCharacter
+      }
+    }
+  
+    askQuestion(){
+      const answer = prompt('Which way to go ? ').toUpperCase();
+      switch (answer){
+        case 'U':
+        this.locationY -=1
+        break;
+        case 'D':
+        this.locationY +=1
+        break;
+        case 'R':
+        this.locationX +=1
+        break;
+        case 'L':
+        this.locationX -=1
+        break;
+        default:
+        console.log('Enter between U,D,R,L')
+        this.askQuestion()
+        break;
+      }
+    }
+
+    isInBounds(){
+return (this.locationY >=0 &&
+this.locationX >=0 && this.locationY < this.field.length
+&& this.locationX < this.field[0].length
+)
+    }
+isHat(){
+  return this.field[this.locationY,this.locationX]===hat
+}
+isHole(){
+  return this.field[this.locationY,this.locationX]=== hole
+}
+
+print (){
+  const displayingString= this.field.map(row=>{
+    return row.join('')
+  }).join('')
+  console.log(displayingString)
+}
+
+static generateField(height,width,percentage=0.1){
+  const field = new Array(height).fill(0).map(el=>new Array(width))
+  for(let y=0;y<height;y++){
+    for(let x=0;x<width;x++){
+      const prob= Math.random()
+field[y][x]= prob > percentage ? fieldCharacter :hole
+    }
+  }
+  const hatLocation = {
+    x: Math.floor(Math.random()*width),
+    y:Math.floor(Math.random()*height)
+  }
+  while(hatLocation.X===0 && hatLocation.y ===0){
+    hatLocation.y =Math.floor(Math.random()*height)
+    hatLocation.x=Math.floor(Math.random()*width)
+  }
+  field[hatLocation.y][hatLocation.x]=hat
+  return field
+}
+}
+const myField = new Field(Field.generateField(10,10,0.2))
+myField.runGame()
